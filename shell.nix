2ba4f46abb523bf789240bd83e4143ead0e34f40
Thomas Marchand <thomas.marchand@tuta.io>
2022-03-26 19:46:31 +0100

    add gmp

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 with import <nixpkgs> { };
 
-let
-  pythonPackages = python3Packages;
+let pythonPackages = python3Packages;
 in pkgs.mkShell rec {
   name = "impurePythonEnv";
   venvDir = "./env";
@@ -13,6 +12,9 @@ in pkgs.mkShell rec {
     # This execute some shell code to initialize a venv in $venvDir before
     # dropping into the shell
     pythonPackages.venvShellHook
+
+    # required by fastecdsa
+    gmp
   ];
 
   # Run this command, only after creating the virtual environment
